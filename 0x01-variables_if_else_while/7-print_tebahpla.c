f63176cc49159823cb8b2900d99efe2e5721c6ef
BankMinistro <ugwumeli7@gmail.com>
2023-06-19 20:10:38 -0700

    reverse alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	char o;
+
+	o = 'z';
+	while
+		(o >= 'a') {
+			putchar(o);
+			o--;
+		}
+	putchar('\n');
+	return (0);
+}
