c6f26d4215e8be093ff7d97ddc719800e91cfa16
BankMinistro <ugwumeli7@gmail.com>
2023-06-20 21:47:56 -0700

    times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *  main - functions nested loops tasks
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int i = 0, b;
+
+	while (i < 10)
+	{
+		b = 'a';
+		while (b <= 'z')
+		{
+			putchar(b);
+			b++;
+		}
+		putchar('\n');
+		i++;
+	}
+	return (0);
+}
