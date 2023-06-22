3ec806da3ce9aba82e7b57683a353bc490623225
BankMinistro <ugwumeli7@gmail.com>
2023-06-21 20:49:41 -0700

    print

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - alx task five
+ * @c:
+ * Return: 1 for positive num, 0 for neg num, else -0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
