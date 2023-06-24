086da191b2ff190e9309f6c71b366eb62a615b75
BankMinistro <ugwumeli7@gmail.com>
2023-06-23 18:42:57 -0700

    primes

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	unsigned long int a, b = 612852475143;
+
+	for (a = 3; a < 782849; a = a + 2)
+	{
+		while ((b % a == 0) && (b != a))
+			b = b / a;
+	}
+	printf("%lu\n", b);
+	return (0);
+}
