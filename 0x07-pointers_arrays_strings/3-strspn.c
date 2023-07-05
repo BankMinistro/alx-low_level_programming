6402d415928d2c17ec1e3c82af7225e62c3e9470
BankMinistro <ugwumeli7@gmail.com>
2023-07-04 19:35:01 -0700

    substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,21 +1,30 @@
 #include "main.h"
 
 /**
- * _strchr - Entry
- * @s: input value
- * @c: input value
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: input
+ * @accept: input
  * Return: Always 0.
  */
 
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
 {
-	int x = 0;
+	unsigned int n = 0;
+	int t;
 
-	for (; s[x] >= '\0'; x++)
+	while (*s)
 	{
-		if (s[x] == c)
-			return (&s[x]);
+		for (t = 0; accept[t]; t++)
+		{
+			if (*s == accept[t])
+			{
+				n++;
+				break;
+			}
+			else if (accept[t + 1] == '\0')
+				return (n);
+		}
+		s++;
 	}
-	return (0);
+	return (n);
 }
-
